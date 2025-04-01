d4f1c95d94a830a000f84c57615d4a8ff243dd73
Luis Gustavo <jesuslara.luis99@gmail.com>
2025-04-01 17:13:40 -0600

    Nuevas Practicas del dia 1

---
+++ 1 DAY PROGRAM A NAME CREATOR/Practica string 1.py
@@ -0,0 +1,8 @@
+#Crea un código que imprima en pantalla la siguiente expresión
+# (pero usando print una sola vez):
+
+#Línea 1
+#Línea 2
+#Línea 3
+
+print("Hello mi name is Luis\nI am twenty-four years old\nI live Fortin")
---
+++ 1 DAY PROGRAM A NAME CREATOR/Practica string 3.py
@@ -0,0 +1,6 @@
+#Crea un código que imprima en pantalla la siguiente expresión:
+#Barra Normal: /
+#Barra Invertida: \
+
+print("Bar Normal: /")
+print("Bar Inverter: \\")
