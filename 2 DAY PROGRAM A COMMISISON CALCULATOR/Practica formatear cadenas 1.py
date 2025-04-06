57f27e879e77e7d037ebed41ad15459ee47ff35f
Luis <jesuslara.luis99@gmail.com>
2025-04-06 14:26:44 -0600

    Solo se agrega practica formatear cadenas 1

---
+++ 2 DAY PROGRAM A COMMISISON CALCULATOR/Practica formatear cadenas 1.py
@@ -0,0 +1,9 @@
+#Necesitamos imprimir el nombre y número de asociado dentro de la siguiente frase:
+
+#Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+
+#Recuerda que la precisión de tu respuesta (espacios, ortografía y puntuación), es muy importante para llegar al resultado correcto.
+
+name = "Jesse Pickman"
+number = 399058
+print(f'Estimado {name}, su numero de asociado es: {number}')
